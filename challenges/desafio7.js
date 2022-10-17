818a3370db745ee0ab5c390e6924661cf153c965
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-10-17 16:28:55 -0300

    Arrumando ESLint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,4 @@
 db.produtos.find({ $and: [
-  { tags: { $exists: false }},
-  { vendidos: { $ne: 50 }}
-]}, {nome: 1, _id: 0, vendidos: 1});
+  { tags: { $exists: false } },
+  { vendidos: { $ne: 50 } },
+] }, { nome: 1, _id: 0, vendidos: 1 });
